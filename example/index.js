4841f0f271362e2bb8e6bc262d027771b25a89b1
Shahriori <ShahTheShah@ya.ru>
2023-04-06 03:53:14 +0300

    Completion of work with the module for finding the determinant of the matrix

--- example/index.js
+++ example/index.js
@@ -2,3 +2,32 @@
 
 import HigherMathJS from '../lib/highermathjs.js';
 
+const N = 19
+
+const matrix = [
+    [10 * N, N, N + 2],
+    [N, 9 * N, 3 * N],
+    [2 * N, 3 * N, 11 * N],
+]
+
+console.log(HigherMathJS.det(matrix, {
+    method: 'gausse'
+}))
+console.log(HigherMathJS.det(matrix, {
+    method: 'minor'
+}))
+console.log(HigherMathJS.det(matrix, {
+    method: 'cramer'
+}))
+console.log(HigherMathJS.det(matrix, {
+    method: 'bareis'
+}))
+console.log(HigherMathJS.det(matrix, {
+    method: 'lu'
+}))
+console.log(HigherMathJS.det(matrix, {
+    method: 'gausse_jordan'
+}))
+console.log(HigherMathJS.det(matrix, {
+    method: 'gausse_croneker'
+}))
