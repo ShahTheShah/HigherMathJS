fad2d43ab2c06bde9eb403964cdf4dc45eabfbc7
Shahriori <ShahTheShah@ya.ru>
2023-04-07 04:13:06 +0300

    Adding 12 methods for calculating the determinant of the matrix

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## HigherMathJS
 
 # Unfortunately, this library is still in the initial stage of development. The functionality that can be provided to the general public is not yet available. The main release is in version 1.0.0!
+
+version 0.0.2 - Adding 12 methods for calculating the determinant of the matrix
--- example/index.js
+++ example/index.js
@@ -9,39 +9,66 @@ const N = 19
 //     [N, 9 * N, 3 * N],
 //     [2 * N, 3 * N, 11 * N],
 // ]
+// const matrix = [
+//     [1, 2.33, 5],
+//     [3.0001, 4.011, 12.3],
+//     [5, 6, 55.44]
+// ]
+
 const matrix = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
+    [4, 2, 6],
+    [2, 10, 9],
+    [6, 9, 14]
 ]
 
-console.log(HigherMathJS.det(matrix, {
-    method: 'gausse'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'minor'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'cramer'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'bareis'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'lu'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'gausse_jordan'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'gausse_croneker'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'det_property'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'laplace'
-}))
-console.log(HigherMathJS.det(matrix, {
-    method: 'block_decompose'
-}))
+const
+    det_gausse = HigherMathJS.det(matrix, {
+        method: 'gausse'
+    }),
+    det_minor = HigherMathJS.det(matrix, {
+        method: 'minor'
+    }),
+    det_cramer = HigherMathJS.det(matrix, {
+        method: 'cramer'
+    }),
+    det_bareis = HigherMathJS.det(matrix, {
+        method: 'bareis',
+        digitAfterPoint: 2
+    }),
+    det_lu = HigherMathJS.det(matrix, {
+        method: 'lu'
+    }),
+    det_gausse_jordan = HigherMathJS.det(matrix, {
+        method: 'gausse_jordan'
+    }),
+    det_gausse_croneker = HigherMathJS.det(matrix, {
+        method: 'gausse_croneker'
+    }),
+    det_property = HigherMathJS.det(matrix, {
+        method: 'det_property'
+    }),
+    det_laplace = HigherMathJS.det(matrix, {
+        method: 'laplace'
+    }),
+    det_block_decompose = HigherMathJS.det(matrix, {
+        method: 'block_decompose'
+    }),
+    det_danzing_wolf = HigherMathJS.det(matrix, {
+        method: 'danzing_wolf'
+    }),
+    det_khaletsky = HigherMathJS.det(matrix, {
+        method: 'khaletsky'
+    });
+
+console.log(det_gausse)
+console.log(det_minor)
+console.log(det_cramer)
+console.log(det_bareis)
+console.log(det_lu)
+console.log(det_gausse_jordan)
+console.log(det_gausse_croneker)
+console.log(det_property)
+console.log(det_laplace)
+console.log(det_block_decompose)
+console.log(det_danzing_wolf)
+console.log(det_khaletsky)
